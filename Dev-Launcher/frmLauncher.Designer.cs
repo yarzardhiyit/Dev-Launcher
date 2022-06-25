2590d8432cc307524d412b6ca8b3702bf17e2fbb
Daorung Waranuphat <yarzardhiyit@gmail.com>
2022-06-25 12:48:07 +0700

    label added

--- Dev-Launcher/frmLauncher.Designer.cs
+++ Dev-Launcher/frmLauncher.Designer.cs
@@ -35,6 +35,7 @@
             this.picVscode = new System.Windows.Forms.PictureBox();
             this.picVscommunity = new System.Windows.Forms.PictureBox();
             this.picIntellij = new System.Windows.Forms.PictureBox();
+            this.lblCopyRight = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picExit)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCmd)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picStackoverflow)).BeginInit();
@@ -47,7 +48,7 @@
             // picExit
             // 
             this.picExit.Image = global::Dev_Launcher.Properties.Resources.exit1;
-            this.picExit.Location = new System.Drawing.Point(654, 13);
+            this.picExit.Location = new System.Drawing.Point(649, 12);
             this.picExit.Name = "picExit";
             this.picExit.Size = new System.Drawing.Size(100, 100);
             this.picExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -58,7 +59,7 @@
             // picCmd
             // 
             this.picCmd.Image = global::Dev_Launcher.Properties.Resources.cmd;
-            this.picCmd.Location = new System.Drawing.Point(548, 13);
+            this.picCmd.Location = new System.Drawing.Point(543, 12);
             this.picCmd.Name = "picCmd";
             this.picCmd.Size = new System.Drawing.Size(100, 100);
             this.picCmd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -69,7 +70,7 @@
             // picStackoverflow
             // 
             this.picStackoverflow.Image = global::Dev_Launcher.Properties.Resources.stackoflow;
-            this.picStackoverflow.Location = new System.Drawing.Point(442, 13);
+            this.picStackoverflow.Location = new System.Drawing.Point(437, 12);
             this.picStackoverflow.Name = "picStackoverflow";
             this.picStackoverflow.Size = new System.Drawing.Size(100, 100);
             this.picStackoverflow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -80,7 +81,7 @@
             // picGit
             // 
             this.picGit.Image = global::Dev_Launcher.Properties.Resources.git;
-            this.picGit.Location = new System.Drawing.Point(336, 13);
+            this.picGit.Location = new System.Drawing.Point(331, 12);
             this.picGit.Name = "picGit";
             this.picGit.Size = new System.Drawing.Size(100, 100);
             this.picGit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -91,7 +92,7 @@
             // picVscode
             // 
             this.picVscode.Image = global::Dev_Launcher.Properties.Resources.vscode;
-            this.picVscode.Location = new System.Drawing.Point(230, 13);
+            this.picVscode.Location = new System.Drawing.Point(225, 12);
             this.picVscode.Name = "picVscode";
             this.picVscode.Size = new System.Drawing.Size(100, 100);
             this.picVscode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -102,7 +103,7 @@
             // picVscommunity
             // 
             this.picVscommunity.Image = global::Dev_Launcher.Properties.Resources.vs_com;
-            this.picVscommunity.Location = new System.Drawing.Point(124, 13);
+            this.picVscommunity.Location = new System.Drawing.Point(119, 12);
             this.picVscommunity.Name = "picVscommunity";
             this.picVscommunity.Size = new System.Drawing.Size(100, 100);
             this.picVscommunity.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -113,7 +114,7 @@
             // picIntellij
             // 
             this.picIntellij.Image = global::Dev_Launcher.Properties.Resources.intellij;
-            this.picIntellij.Location = new System.Drawing.Point(18, 13);
+            this.picIntellij.Location = new System.Drawing.Point(13, 12);
             this.picIntellij.Name = "picIntellij";
             this.picIntellij.Size = new System.Drawing.Size(100, 100);
             this.picIntellij.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -121,13 +122,23 @@
             this.picIntellij.TabStop = false;
             this.picIntellij.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // lblCopyRight
+            // 
+            this.lblCopyRight.AutoSize = true;
+            this.lblCopyRight.Location = new System.Drawing.Point(12, 128);
+            this.lblCopyRight.Name = "lblCopyRight";
+            this.lblCopyRight.Size = new System.Drawing.Size(213, 16);
+            this.lblCopyRight.TabIndex = 7;
+            this.lblCopyRight.Text = "Made with ❤️ by yarzardhiyit © 2022";
+            // 
             // frmLauncher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(773, 126);
+            this.ClientSize = new System.Drawing.Size(762, 153);
             this.ControlBox = false;
+            this.Controls.Add(this.lblCopyRight);
             this.Controls.Add(this.picExit);
             this.Controls.Add(this.picCmd);
             this.Controls.Add(this.picStackoverflow);
@@ -145,6 +156,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.picVscommunity)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picIntellij)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -157,6 +169,7 @@
         private System.Windows.Forms.PictureBox picCmd;
         private System.Windows.Forms.PictureBox picStackoverflow;
         private System.Windows.Forms.PictureBox picExit;
+        private System.Windows.Forms.Label lblCopyRight;
     }
 }
 
